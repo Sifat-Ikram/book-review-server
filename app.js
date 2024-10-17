3efe689c99cacb7626f4c546c6ebbcb382f73668
Sifat-Ikram <sifatikram@gmail.com>
2024-10-17 22:46:46 +0600

    server side completed

--- app.js
+++ app.js
@@ -8,8 +8,29 @@ const reviewRoutes = require("./routes/reviewRoutes");
 const bookRoutes = require("./routes/bookRoutes");
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+        "https://book-house-1234.web.app",
+        "https://book-house-1234.firebaseapp.com",
+      ],
+      methods: ["GET", "POST", "PATCH", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  );
+  
 app.use(express.json());
+
+// jwt api
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = await jwt.sign(user, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  res.send({ token });
+});
 
 // Routes
 app.use(userRoutes);
